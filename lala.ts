fbf519ea428ffd8fa28dc553c34e6f43b0d95224
hector.arrechea <hector.arrechea@ct.uneatlantico.es>
2023-10-30 03:15:16 +0100

    feature: create index.ts for general exports

---
+++ lala.ts
@@ -0,0 +1,3 @@
+import hola from './src';
+
+hola.httpErrorCodes.;
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
+import * as client from "./errors/client";
+import * as redirects from "./errors/redirects";
+import * as server from "./errors/server";
+import { isHttpError, createError } from "./errors";
 
-export * from './errors';
+export default {
+    httpErrors: {
+        ...client,
+        ...redirects,
+        ...server,
+    },
+    isHttpError,
+    createError
+};

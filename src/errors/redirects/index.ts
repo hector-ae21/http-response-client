b21acf029a41e66b42b6e8d96af2f6878db381df
hector.arrechea <hector.arrechea@ct.uneatlantico.es>
2023-10-30 03:14:01 +0100

    fix: Sorting exports by status code

--- src/errors/redirects/index.ts
+++ src/errors/redirects/index.ts
@@ -1,9 +1,8 @@
-export { default as Found } from "./found";
-export { default as MovedPermanently } from "./moved-permanently";
-export { default as PermanentRedirect } from "./permanent-redirect";
-export { default as SeeOther } from "./see-other";
-export { default as TemporaryRedirect } from "./temporary-redirect";
-export { default as UseProxy } from "./use-proxy";
-export { default as MultipleChoice } from "./multiple-choice";
-export { default as NotModified } from "./not-modified";
-export { default as Unused } from "./unused";
+export { default as MultipleChoice } from "./multiple-choice"; // 300
+export { default as MovedPermanently } from "./moved-permanently"; // 301
+export { default as Found } from "./found"; // 302
+export { default as SeeOther } from "./see-other"; // 303
+export { default as NotModified } from "./not-modified"; // 304
+export { default as UseProxy } from "./use-proxy"; // 305
+export { default as TemporaryRedirect } from "./temporary-redirect"; // 307
+export { default as PermanentRedirect } from "./permanent-redirect"; // 308
